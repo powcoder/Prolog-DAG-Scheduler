c99eea9a6fec67dea437c5fd778fe75c7eb54a3e
powcoder <powcoder@163.com>
2022-11-30 14:42:32 +0800

    Prolog DAG Scheduler

---
+++ src/main.pl
@@ -0,0 +1,37 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                    %
+%  Declarative Programming Project:  %
+%       Task-Parallel Scheduler      %
+%                                    %
+%           Youri Coppens            %
+%                                    %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%% HOW TO RUN %%%
+
+/*****************
+
+1) Open terminal/command line in the project source folder 'src'
+2) Run the following command: swipl ../data/<instance-file> main.pl
+3) Enjoy querying!
+
+*****************/
+
+:- module(main,[]).
+:- reexport([ speedup,
+              find_optimal,
+              find_heuristically,
+              pretty_print
+            ]).
+:- reexport(solution, except([solution/1])).
+:- reexport(execution_time, except([ task_term_time/3, 
+                                     memo_task_term_time/2
+                                   ])).
